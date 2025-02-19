194f6b653e75f6a29fa8481a4ab51ba945e39ca8
MinhDang1701 <minh82320@gmail.com>
2025-02-19 10:21:53 +0700

    Update Processor.java

--- Processor.java
+++ Processor.java
@@ -3,11 +3,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package testbaitap;
+       import java.util.Date;
 
 /**
  *
  * @author ThinkPad
  */
 public class Processor {
-    
+       public static void main(String[] args) {
+        PersonList personList = new PersonList();
+
+        personList.addPerson(new Student("S001", "City A", new Date(), "Computer Science"));
+        personList.addPerson(new Teacher("T001", "City B", new Date(), "Mathematics"));
+
+        personList.displayAll();
+    }
 }
